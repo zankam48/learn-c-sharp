dde087308dd4bf8209785ee1598eb5b54569e2cc
Hikamizan <hikamizan771@gmail.com>
2023-05-21 15:16:17 +0700

    commit oop basic

---
+++ oop/heroes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_basic.oop
+{
+    class Heroes
+    {
+        public string name;
+        public string type;
+        public int power;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heroes heroes1 = new Heroes();
+            heroes1.name = "Hikamizan";
+            Console.WriteLine(heroes1.name);
+        }
+    }
+    
+}
+
+
