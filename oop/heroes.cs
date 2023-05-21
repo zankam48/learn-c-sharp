22be9772f48acc5f52929e9ccc720704a3a7ef84
Hikamizan <hikamizan771@gmail.com>
2023-05-21 15:52:04 +0700

    nambahin set dan get

--- oop/heroes.cs
+++ oop/heroes.cs
@@ -11,24 +11,27 @@ namespace oop_basic.oop
         public string name;
         public string type;
         public int power;
+        private string gender;
 
-        public Heroes(string aName, string aType, int aPower = 0)
+        public Heroes(string aName, string aType, string aGender, int aPower = 0)
         {
             name = aName;
             type = aType;
             power = aPower;
+            Gender = aGender;
 
-            Console.WriteLine(name + " " + type + " " + power);
+
+            Console.WriteLine(name + " " + type + " " + power + " " + Gender);
         }
 
         public string isStrong()
         {
             string powerScale;
-            if ( power > 100)
+            if (power > 100)
             {
                 powerScale = "he is very strong";
             }
-            else if ( power > 50 && power < 100)
+            else if (power > 50 && power < 100)
             {
                 powerScale = "he is ordinary";
             }
@@ -38,13 +41,29 @@ namespace oop_basic.oop
             }
             return powerScale;
         }
+
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == "male" || value == "female")
+                {
+                    gender = value;
+                }
+                else
+                {
+                    gender = "im mental illness";
+                }
+            }
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Heroes heroes1 = new Heroes("hikamizna", "warrior", 20);
+            Heroes heroes1 = new Heroes("hikamizna", "warrior", "male", 20);
             Console.WriteLine(heroes1.isStrong());
         }
     }
