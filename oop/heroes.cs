13f60d79f1511e7a141ec161823aa7353ee22416
Hikamizan <hikamizan771@gmail.com>
2023-05-21 17:16:37 +0700

    Menambah gethealth dan healthup

--- oop/heroes.cs
+++ oop/heroes.cs
@@ -10,18 +10,32 @@ namespace oop_basic.oop
     {
         public string name;
         public string type;
-        public int power;
-        private string gender;
 
-        public Heroes(string aName, string aType, string aGender, int aPower = 0)
+        public int power;
+        public int health;
+        public int defense;
+
+        private string gender;
+        public static int heroesCount = 0;
+        private HeroesClass heroesClass { get; set; }
+
+        public Heroes(string aName, string aType, string aGender, HeroesClass aHeroesClass, int aHealth, int aPower, int aDefense)
         {
             name = aName;
             type = aType;
+
             power = aPower;
+            health = aHealth;
+            defense = aDefense; 
+
             Gender = aGender;
+            heroesClass = aHeroesClass;
+            heroesCount++;
+        }
 
-
-            Console.WriteLine(name + " " + type + " " + power + " " + Gender);
+        public void getInfo()
+        {
+            Console.WriteLine(name + " " + type + " " + Gender + " " + heroesClass + " " + health + " " + power + " " + defense);
         }
 
         public string isStrong()
@@ -42,6 +56,16 @@ namespace oop_basic.oop
             return powerScale;
         }
 
+        public void healthUp(int up)
+        {
+            health += up;
+        }
+
+        public int getHealth()
+        {
+            return health;
+        }
+
         public string Gender
         {
             get { return gender; }
@@ -57,14 +81,42 @@ namespace oop_basic.oop
                 }
             }
         }
+
+        public int getHeroesCount()
+        {
+            return heroesCount;
+        }
     }
+
+    public enum HeroesClass
+    {
+        Warrior,
+        Archer,
+        Mage,
+        Berserker
+    }
+
+    //public class Mage : Heroes
+    //{
+    //    public void 
+    //}
 
     class Program
     {
         static void Main(string[] args)
         {
-            Heroes heroes1 = new Heroes("hikamizna", "warrior", "male", 20);
-            Console.WriteLine(heroes1.isStrong());
+            Heroes heroes1 = new Heroes("hikamizna", "warrior", "malexx", HeroesClass.Archer, 100, 30, 60);
+            Heroes heroes2 = new Heroes("hikamiznax", "assassin", "male", HeroesClass.Mage, 120, 20, 50);
+            Heroes heroes3 = new Heroes("hikamiznay", "archer", "female", HeroesClass.Berserker, 80, 40, 70);
+            /*Console.WriteLine(heroes1.isStrong());
+            Console.WriteLine(heroes2.isStrong());
+            Console.WriteLine(heroes3.isStrong());
+            Console.WriteLine(Heroes.heroesCount);
+            Console.WriteLine(heroes1.getHeroesCount());*/
+
+            heroes1.getInfo();
+            Console.WriteLine(heroes1.getHealth()); 
+
         }
     }
     
